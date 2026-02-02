24a81903254e63b4aa65915b7f1ed341a1d4a184
thanhdanhkode <bfffamilk@gmail.com>
2026-02-02 14:58:43 +0700

    refactor: change to astro
    BREAKING CHANGE: Convert web to Astro.js

---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+// @ts-check
+
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://thanhdanh.me",
+  integrations: [sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  adapter: cloudflare(),
+});
